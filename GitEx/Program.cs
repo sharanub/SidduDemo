79e2ccc3a48893502104f6b0328b593e61c3d7c8
Sharanu <sharanu_b@hotmail.com>
2024-03-01 22:32:47 +0530

    Add project files.

---
+++ GitEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
